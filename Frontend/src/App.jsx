105a7693023cd0ead524d98ea8dbe3b99d7504b8
abhisheksharma226 <avisheksharma2004@gmail.com>
2024-12-10 22:04:27 +0530

    added components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+// import Home from "./pages/Home"
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-   <h1>Project init</h1>
+    <h1>Home</h1>
   )
 }
 
